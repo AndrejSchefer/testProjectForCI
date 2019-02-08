b2d01fb9c77c1fd35c89a5ef7248a9ef3cc489b9
root <root@v22019015686982171.bestsrv.de>
2019-02-08 07:09:24 +0100

    change Dockerfil

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        Hi  There .............
+            Hi  There .............     ^CSD
+            CFWDS
+            
         </header>
       </div>
     );
